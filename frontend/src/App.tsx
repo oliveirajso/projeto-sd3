105f24492a60250df2326be7b3ba46840ce065bb
Jefferson <jefferson.admsistemas@gmail.com>
2021-05-09 02:01:56 -0300

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,6 @@
-import ReactGA from "react-ga";
 import Routes from "Routes";
 
 function App() {
-  ReactGA.initialize("G-14MQ1M8WEK");
-  ReactGA.pageview(window.location.pathname + window.location.search);
-
   return <Routes />;
 }
 
